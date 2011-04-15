24ec9fbbecc7c7764d74ec0a2c37f19f43975060
Allen <aibara@jobyenergy.com>
2011-04-14 17:17:31 -0700

    Updates to compile with makani config

--- makani_compile.sh
+++ makani_compile.sh
@@ -1,4 +1,5 @@
 #!/bin/sh 
 
+make KBUILD_DEFCONFIG=makani_defconfig ARCH=arm CROSS_COMPILE=arm-none-linux-gnueabi- defconfig
 make ARCH=arm CROSS_COMPILE=arm-none-linux-gnueabi- uImage
 make ARCH=arm CROSS_COMPILE=arm-none-linux-gnueabi- modules
